78a60b4ad7903b0e860517921a70915fbddb4c08
Eugene Aldoshkin <aldoshkineg@gmail.com>
2026-06-10 14:23:14 +0300

    fix: correct endpoints syntax in S3 backend config

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -4,7 +4,9 @@ terraform {
     bucket                      = "atlas-tf-7612"
     key                         = "terraform/atlas-idp/terraform.tfstate"
     region                      = "eu-central-003"
-    endpoints.s3                = "https://s3.eu-central-003.backblazeb2.com"
+    endpoints = {
+      s3 = "https://s3.eu-central-003.backblazeb2.com"
+    }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
