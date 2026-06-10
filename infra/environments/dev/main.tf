aade04b20f6da06901fd0a5a57d93353d6abc6e1
Eugene Aldoshkin <aldoshkineg@gmail.com>
2026-06-10 14:21:43 +0300

    fix: add skip_requesting_account_id and use endpoints.s3 for B2 backend

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -4,10 +4,11 @@ terraform {
     bucket                      = "atlas-tf-7612"
     key                         = "terraform/atlas-idp/terraform.tfstate"
     region                      = "eu-central-003"
-    endpoint                    = "https://s3.eu-central-003.backblazeb2.com"
+    endpoints.s3                = "https://s3.eu-central-003.backblazeb2.com"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
   }
 }
 
